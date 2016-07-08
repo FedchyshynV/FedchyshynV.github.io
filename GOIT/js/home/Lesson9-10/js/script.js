a6f83a767e2fa2b87af0d6b824e14395b7ec7f68
Fechyshyn Vladymyr <fedchyshyn.v@gmail.com>
2016-07-08 13:25:53 +0300

    add js lesson9-10

---
+++ GOIT/js/home/Lesson9-10/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Vova on 06.07.2016.
+ */
+
+
+$(function() {
+    $( '.dropdown' ).hover(
+        function(){
+            $(this).children('.sub-menu').slideDown(500);
+        },
+        function(){
+            $(this).children('.sub-menu').slideUp(300);
+        }
+    );
+});
+
+
+//////////////// select////
+
+$(function() {
+    // initialize sol
+    $('#my-select').searchableOptionList();
+});
