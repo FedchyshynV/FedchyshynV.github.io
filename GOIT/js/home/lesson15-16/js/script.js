a201db87c79a39c9b0eb5a09f5628b35524e64d9
Fechyshyn Vladymyr <fedchyshyn.v@gmail.com>
2016-08-16 18:01:34 +0300

    add js lesson15-16

--- GOIT/js/home/lesson15-16/js/script.js
+++ GOIT/js/home/lesson15-16/js/script.js
@@ -35,117 +35,81 @@ $(function() {
 });
 
 
+//Homework lesson 16/////////////////////////////////////////////////////////////////
+
 function Human() {
-    this.name = 'name';
-    this.age = 0;
-    this.gender = 'gender';
-    this.height = 0;
-    this.weight = 0;
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.height = height;
+    this.weight = weight;
 }
 
-function Worker() {
-    this.job = '';
-    this.salary = 0;
+function Worker(job,salary) {
+    this.job = job;
+    this.salary = salary;
     this.work = function(){
-        return 'go to work every morning.';
+        console.log( " had job in:", this.job,
+            "\n had salary in USD:", this.salary,
+            "\n go to work every morning.");
+        console.log("*********************************");
     };
 }
 
-function Student() {
-    this.study = '';
-    this.grants = 0;
+function Student(study,grants) {
+    this.study = study;
+    this.grants = grants;
     this.watchTv = function() {
-        return 'watch TV soap opera all day.';
+        console.log(" had study in:", this.study,
+            "\n had grants in USD:", this.grants,
+            "\n watch TV all day.");
+        console.log("*********************************");
     };
+}
+
+Human.prototype.Visual = function(){
+    console.log('Worker name is',this.name,
+        "\n age:", this.age,
+        "\n gender:", this.gender,
+        "\n height:", this.height,
+        "\n weight:", this.weight)
 }
 
 Worker.prototype = Object.create(Human.prototype);
 Student.prototype = Object.create(Human.prototype);
 
-
-
-var worker1 = new Worker();
-worker1.name = 'Vasia';
-worker1.age = 35;
+var worker1 = new Worker('Police',1000);
+worker1.name = 'Vasya';
+worker1.age = 28;
 worker1.gender  = "man";
-worker1.height  = 185;
-worker1.weight  = 98;
-worker1.job = 'Frontend developer';
-worker1.salary = 2000;
+worker1.height  = 180;
+worker1.weight  = 80;
+worker1.Visual();
+worker1.work();
 
-var worker2 = new Worker();
-worker2.name = 'Petia';
-worker2.age = 45;
+var worker2 = new Worker('Developer',3500);
+worker2.name = 'Petya';
+worker2.age = 29;
 worker2.gender  = "man";
-worker2.height  = 177;
-worker2.weight  = 85;
-worker2.job = 'Java developer';
-worker2.salary = 3000;
+worker2.height  = 181;
+worker2.weight  = 81;
+worker2.Visual();
+worker2.work();
 
-var student1 = new Student();
-student1.name = "Igor";
-student1.age = 19;
+var student1 = new Student('Garvard',500);
+student1.name = "John";
+student1.age = 21;
 student1.gender = "man";
-student1.height = 165;
-student1.weight = 60;
-student1.study = 'Oksford';
-student1.grants = 250;
+student1.height = 175;
+student1.weight = 70;
+student1.Visual();
+student1.watchTv();
 
-var student2 = new Student();
-student2.name = "Katia";
-student2.age = 18;
+var student2 = new Student('Oksford',350);
+student2.name = "Alicia";
+student2.age = 19;
 student2.gender = "women";
-student2.height = 160;
-student2.weight = 52;
-student2.study = 'Garvard';
-student2.grants = 200;
-
-console.log('Worker name is',worker1.name,
-    "\n age:", worker1.age,
-    "\n gender:", worker1.gender,
-    "\n height:", worker1.height,
-    "\n weight:", worker1.weight,
-    "\n had job in:", worker1.job,
-    "\n had salary in USD:", worker1.salary
-);
-console.log(worker1.name, worker1.work());
-
-console.log ("---------------------------------")
-
-console.log('Worker name is',worker2.name,
-    "\n age:", worker2.age,
-    "\n gender:", worker2.gender,
-    "\n height:", worker2.height,
-    "\n weight:", worker2.weight,
-    "\n had job in:", worker2.job,
-    "\n had salary in USD:", worker2.salary
-);
-console.log(worker2.name, worker2.work());
-
-console.log ("---------------------------------");
-
-
-
-console.log('Student name is',student1.name,
-    "\n age:", student1.age,
-    "\n gender:", student1.gender,
-    "\n height:", student1.height,
-    "\n weight:", student1.weight,
-    "\n he is study in:", student1.study,
-    "\n had grants in USD:", student1.grants
-);
-console.log(student1.name,student1.watchTv());
-
-console.log ("---------------------------------");
-
-
-
-console.log('Student name is',student2.name,
-    "\n age:", student2.age,
-    "\n gender:", student2.gender,
-    "\n height:", student2.height,
-    "\n weight:", student2.weight,
-    "\n he is study in:", student2.study,
-    "\n had grants in USD:", student2.grants
-);
-console.log(student2.name,student2.watchTv());
+student2.height = 170;
+student2.weight = 55;
+student2.Visual();
+student2.watchTv();
