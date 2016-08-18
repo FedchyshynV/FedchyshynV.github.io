c9c7d917c8f893759c8875cc511ef13692300044
Fechyshyn Vladymyr <fedchyshyn.v@gmail.com>
2016-08-18 18:12:35 +0300

    add js lesson15-16

--- GOIT/js/home/lesson15-16/js/script.js
+++ GOIT/js/home/lesson15-16/js/script.js
@@ -32,84 +32,57 @@ $(function() {
         console.log($(this));
     });
 
+
+
+
+
+
+
 });
 
 
 //Homework lesson 16/////////////////////////////////////////////////////////////////
 
-function Human() {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.height = height;
-    this.weight = weight;
+
+var human = {
+    name: '',
+    age: 0,
+    gender: '',
+    height: 0,
+    weight: 0
 }
 
-function Worker(job,salary) {
-    this.job = job;
-    this.salary = salary;
-    this.work = function(){
-        console.log( " had job in:", this.job,
-            "\n had salary in USD:", this.salary,
-            "\n go to work every morning.");
-        console.log("*********************************");
-    };
+var worker = {
+    job: '',
+    salary: 0,
+    work: function() {
+        console.log('go to the work every day');
+    }
 }
 
-function Student(study,grants) {
-    this.study = study;
-    this.grants = grants;
-    this.watchTv = function() {
-        console.log(" had study in:", this.study,
-            "\n had grants in USD:", this.grants,
-            "\n watch TV all day.");
-        console.log("*********************************");
-    };
+var student = {
+    university: '',
+    grands: 0,
+    watchSerials: function() {
+        console.log('watch tv oll day');
+    }
 }
 
-Human.prototype.Visual = function(){
-    console.log('Worker name is',this.name,
-        "\n age:", this.age,
-        "\n gender:", this.gender,
-        "\n height:", this.height,
-        "\n weight:", this.weight)
-}
+worker.__proto__ = human;
+student.___proto__ = human;
 
-Worker.prototype = Object.create(Human.prototype);
-Student.prototype = Object.create(Human.prototype);
-
-var worker1 = new Worker('Police',1000);
-worker1.name = 'Vasya';
-worker1.age = 28;
-worker1.gender  = "man";
-worker1.height  = 180;
-worker1.weight  = 80;
-worker1.Visual();
-worker1.work();
-
-var worker2 = new Worker('Developer',3500);
-worker2.name = 'Petya';
-worker2.age = 29;
-worker2.gender  = "man";
-worker2.height  = 181;
-worker2.weight  = 81;
-worker2.Visual();
-worker2.work();
-
-var student1 = new Student('Garvard',500);
-student1.name = "John";
-student1.age = 21;
-student1.gender = "man";
-student1.height = 175;
-student1.weight = 70;
-student1.Visual();
-student1.watchTv();
-
-var student2 = new Student('Oksford',350);
-student2.name = "Alicia";
-student2.age = 19;
-student2.gender = "women";
-student2.height = 170;
-student2.weight = 55;
-student2.Visual();
-student2.watchTv();
+var vasya = worker;
+vasya.name = 'Jhon';
+vasya.age = 30;
+vasya.job = "Dev";
+vasya.salary = "300$"
+vasya.work();
+console.log(vasya);
+console.log("*******************************")
+var petya = student;
+petya.name = 'Petya';
+petya.age = 20;
+petya.university = "VNTU";
+petya.grands= "300$"
+petya.watchSerials();
+console.log(petya);
