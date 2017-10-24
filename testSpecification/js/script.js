093ae04809926186f8c86b55a28fbf2035afc875
Fedchyshyn Volodymyr <fedchyshyn.v@gmail.com>
2017-10-25 00:59:07 +0300

    release

--- testSpecification/js/script.js
+++ testSpecification/js/script.js
@@ -11,3 +11,23 @@ $('.languages li').click( function(){
     }
 });
 
+$(document).ready(function() {
+    $( '.dropdown' ).hover(
+        function(){
+            $(this).children('.dropDownMenu').slideDown(200);
+        },
+        function(){
+            $(this).children('.dropDownMenu').slideUp(200);
+        }
+    );
+
+
+});
+
+    var h = $( '.block_1' ).height();
+   $( '.block_2' ).height(h);
+$( window ).resize(function() {
+   var h = $( '.block_1' ).height();
+   $( '.block_2' ).height(h);
+});
+
